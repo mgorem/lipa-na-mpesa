54f5c4a2f480cc9e38b7e90ce37fc3ceaf4638c7
omg <oremgitonga22@gmail.com>
2022-11-21 12:51:35 +0300

    server response pushing stk perfectly

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 
 const app = express();
+const cors = require("cors");
+
 const TokenRoute = require("./routes/token");
 
 app.listen(5000, () => {
   console.log("Server Running Perfectly!");
 });
 
+app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Mpesa integration in progress, time to get paid!");
 });
